f5bb2f542f5be94f9628bf2ca8e92f6090bb72e6
Parag Sane <105410724+ParagSa@users.noreply.github.com>
2023-02-02 14:49:40 +0530

    third commit

---
+++ parag.java
@@ -0,0 +1,20 @@
+/**
+ * parag
+ */
+public class parag {
+    public static void main(String[] args) {
+        int[] arr = {30,49,80,12,45};
+        int search = 80;
+
+
+        for(int i = 0; i<arr.length;i++){
+            if(arr[i]==search){
+                System.out.println("present");
+            }
+           
+
+        }
+    }
+
+    
+}
